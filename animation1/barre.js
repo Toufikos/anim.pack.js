37c739238c96314f19d6d0494dfca9f8ce7ffa51
Toufikos <toufik.yahyaoui@outlook.fr>
2021-03-01 18:08:54 +0100

    ajouts

---
+++ animation1/barre.js
@@ -0,0 +1,22 @@
+var pos = 0, 
+    idBarre = null;
+ 
+
+
+function myMove() {
+  var elem = document.getElementById("animate");   
+
+  clearInterval(idBarre);
+
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 750) {
+      clearInterval(id);
+    } else {
+
+      pos++; 
+      elem.style.top = pos + "px"; 
+      elem.style.left = pos + "px"; 
+    }
+  }
+}
